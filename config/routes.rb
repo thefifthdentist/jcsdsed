a20878d681626b34bd439a092cab40d5a0bee9c5
thefifthdentist <37271419+thefifthdentist@users.noreply.github.com>
2018-05-21 17:36:18 -0400

    come on git

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :teachers
+  resources :students
+  resources :cohorts
+  resources :courses
+  resources :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
